d7ce36b0e97c02f35fb17e5e7d212b2b7c8c6f3d
Colin Prince <col@colinprince.com>
2015-10-03 23:00:34 -0400

    Initial Commit

---
+++ LearnOC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LearnOC
+//
+//  Created by Colin Prince on 2015-10-03.
+//  Copyright © 2015 Colin Prince. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
